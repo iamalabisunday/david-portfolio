87251b8bfbba03aaec73c79b99e133bdd092fc04
Sunday Alabi <iamalabisunday@gmail.com>
2025-09-14 14:53:40 -0700

    initial commit

--- src/pages/contactandinfopage.tsx
+++ src/pages/contactandinfopage.tsx
@@ -37,62 +37,64 @@ export default function ContactAndInfoPage() {
           <span className="text-primary font-bold text-xl text-[2rem]">
             Get in Touch
           </span>
-          <form
-            action="submit"
-            className="max-w-[25rem] flex flex-col gap-4 text-[1rem] font-medium leading-[2rem]"
-          >
-            {/* Full name Section */}
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor="name"
-                className="text-priGray text-[1.25rem] font-[400]"
-              >
-                Full name
-              </label>
-              <input
-                type="text"
-                id="name"
-                className="w-full py-[0.25rem] px-[1rem] outline-none border-2 rounded-sm border-priGray focus:border-2 focus:border-active"
-              />
-            </div>
+          <div className="w-full h-full flex justify-between items-center gap-4">
+            <form
+              action="submit"
+              className="lg:max-w-[25rem] w-full flex flex-col gap-4 text-[1rem] font-medium leading-[2rem]"
+            >
+              {/* Full name Section */}
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="name"
+                  className="text-priGray text-[1.25rem] font-[400]"
+                >
+                  Full name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-full py-[0.25rem] px-[1rem] outline-none border-2 rounded-sm border-priGray focus:border-2 focus:border-active"
+                />
+              </div>
 
-            {/* Email Section */}
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor="email"
-                className="text-priGray text-[1.25rem] font-[400]"
-              >
-                Enter Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full py-[0.25rem] px-[1rem] outline-none border-2 rounded-sm border-priGray focus:border-2 focus:border-active"
-              />
-            </div>
+              {/* Email Section */}
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="email"
+                  className="text-priGray text-[1.25rem] font-[400]"
+                >
+                  Enter Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full py-[0.25rem] px-[1rem] outline-none border-2 rounded-sm border-priGray focus:border-2 focus:border-active"
+                />
+              </div>
 
-            {/* Message Section */}
-            <div className="flex flex-col gap-2">
-              <label
-                htmlFor="message"
-                className="text-priGray text-[1.25rem] font-[400]"
-              >
-                Message
-              </label>
-              <textarea
-                name="message"
-                id="message"
-                className="w-full h-[7rem] py-[0.25rem] px-[1rem] outline-none border-2 rounded-sm border-priGray focus:border-2 focus:border-active"
-              ></textarea>
-            </div>
+              {/* Message Section */}
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="message"
+                  className="text-priGray text-[1.25rem] font-[400]"
+                >
+                  Message
+                </label>
+                <textarea
+                  name="message"
+                  id="message"
+                  className="w-full h-[7rem] py-[0.25rem] px-[1rem] outline-none border-2 rounded-sm border-priGray focus:border-2 focus:border-active"
+                ></textarea>
+              </div>
 
-            {/* Button */}
-            <div className="flex justify-end">
-              <button className="text-white text-[1.25rem] font-[400] bg-priGray px-8 py-[0.3rem] rounded-sm">
-                Submit
-              </button>
-            </div>
-          </form>
+              {/* Button */}
+              <div className="flex justify-end">
+                <button className="text-white text-[1.25rem] font-[400] bg-priGray px-8 py-[0.3rem] rounded-sm">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
