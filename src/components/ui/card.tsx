8e3e3b0526a4c732c903d755a390f2c138c18c17
Sunday Alabi <iamalabisunday@gmail.com>
2025-09-15 00:31:48 -0700

    initial commit

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,32 +1,37 @@
 // import { Navlink } from "react-router-dom";
-import Affiliate from "@/assets/affiliate/affiliate.png";
-import AELogo from "@/assets/affiliate/AEJuice.svg";
 
-export default function Card() {
+type CardProps = {
+  img: string;
+  logo: string;
+  title: string;
+  description: string;
+  buttonText: string;
+};
+
+export default function Card({
+  img,
+  logo,
+  title,
+  description,
+  buttonText,
+}: CardProps) {
   return (
     <div className="lg:max-w-[22rem] w-full h-auto border-1 border-border">
       {/* Image Section */}
       <div className="w-full">
-        <img
-          src={Affiliate}
-          alt="Affiliate image"
-          className="object-cover w-full"
-        />
+        <img src={img} alt="Affiliate image" className="object-cover w-full" />
       </div>
       {/* content Section */}
       <div className="w-full py-[1.5rem] px-[1rem] rounded-b-[8px] flex flex-col gap-[0.75rem]">
         <img
-          src={AELogo}
+          src={logo}
           alt="AEJuice Logo"
           className="w-[5rem] flex items-start justify-start"
         />
-        <p className="text-[1.5rem]">Bring your art to life</p>
-        <p className="text-[1.25rem] text-priGray">
-          AEJuice gives you powerful tools and effects to animate faster, create
-          better, and wow your audience.
-        </p>
+        <p className="text-[1.5rem]">{title}</p>
+        <p className="text-[1.25rem] text-priGray">{description}</p>
         <div className="w-full flex justify-end hover:text-priGray cursor-pointer transition-all duration-300">
-          <p>Get AEJuice Now</p>
+          <p>{buttonText}</p>
           <span className="material-symbols-outlined">chevron_right</span>
         </div>
       </div>
--- src/pages/affiliatepage.tsx
+++ src/pages/affiliatepage.tsx
@@ -1,9 +1,24 @@
 import Card from "../components/ui/card.tsx";
+import Affiliate from "@/assets/affiliate/affiliate.png";
+import AELogo from "@/assets/affiliate/AEJuice.svg";
 
 export default function AffiliatePage() {
   return (
-    <div className="flex justify-center lg:justify-start">
-      <Card />
+    <div className="flex justify-center lg:justify-start gap-5">
+      <Card
+        img={Affiliate}
+        logo={AELogo}
+        title="Bring your art to life"
+        description="AEJuice gives you powerful tools and effects to animate faster, create better, and wow your audience."
+        buttonText="Get AEJuice Now"
+      />
+      <Card
+        img={Affiliate}
+        logo={AELogo}
+        title="Love you more"
+        description="AEJuice gives you powerful tools and effects to animate faster, create better, and wow your audience."
+        buttonText="Get AEJuice Now"
+      />
     </div>
   );
 }
