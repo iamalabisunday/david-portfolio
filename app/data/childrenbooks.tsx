e97cad86cec982382c8db212834153b3e9dd47ce
Sunday Alabi <iamalabisunday@gmail.com>
2025-09-25 12:51:33 -0700

    initial commit

--- app/data/childrenbooks.tsx
+++ app/data/childrenbooks.tsx
@@ -2,7 +2,7 @@ const childrenBooks = [
   {
     id: 1,
     title: "Ammy Kiwi",
-    img: "/AmmyKiwi.jpg",
+    img: "/children/AmmyKiwi.jpg",
     destination: "/childrenbooks/ammykiwi",
     message:
       "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
@@ -12,7 +12,7 @@ const childrenBooks = [
   {
     id: 2,
     title: "Golden",
-    img: "/Golden.jpg",
+    img: "/children/Golden.jpg",
     destination: "/childrenbooks/golden",
     message:
       "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
@@ -22,7 +22,7 @@ const childrenBooks = [
   {
     id: 3,
     title: "Woods",
-    img: "/Woods.jpg",
+    img: "/children/Woods.jpg",
     destination: "/childrenbooks/wood",
     message:
       "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
@@ -32,7 +32,167 @@ const childrenBooks = [
   {
     id: 4,
     title: "Zoo",
-    img: "/Zoo.jpg",
+    img: "/children/Zoo.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 5,
+    title: "BaABlessing",
+    img: "/children/BaABlessing.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 6,
+    title: "BeNice",
+    img: "/children/BeNice.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 7,
+    title: "BePrayerful",
+    img: "/children/BePrayerful.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 8,
+    title: "BePeaceful",
+    img: "/children/BePeaceful.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 9,
+    title: "Boldness",
+    img: "/children/Boldness.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 10,
+    title: "ChildrenOfGod",
+    img: "/children/ChildrenOfGod.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 11,
+    title: "Faith",
+    img: "/children/Faith.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 12,
+    title: "Bilble",
+    img: "/children/Bilble.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 13,
+    title: "Tickets",
+    img: "/children/Tickets.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 14,
+    title: "Map",
+    img: "/children/Map.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 15,
+    title: "Africa",
+    img: "/children/Africa.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 16,
+    title: "Bald",
+    img: "/children/Bald.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 17,
+    title: "Creation",
+    img: "/children/Creation.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 18,
+    title: "Car",
+    img: "/children/Car.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 19,
+    title: "CarChild",
+    img: "/children/CarChild.jpg",
+    destination: "/childrenbooks/zoo",
+    message:
+      "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
+    paragrapy:
+      "Don’t forget to keep an eye out for special clues along the way! Let the adventure begin and may it never end…",
+  },
+  {
+    id: 20,
+    title: "Ntarctica",
+    img: "/children/Ntarctica.jpg",
     destination: "/childrenbooks/zoo",
     message:
       "Go on an adventure with a little girl and her pup as they make-believe their way through the zoo! Pack your adventure bag with all the necessities and discover all kinds of fun animals that live around the world! Ammy and Kiwi are the best of friends and love learning together and making memories.",
